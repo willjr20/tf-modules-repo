8b2266a01ee48618037979e3975426dae077e897
William <willjr20@gmail.com>
2022-11-15 22:56:38 -0500

    Created new module directory

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "az_rg_name" {
-  value       = azurerm_resource_group.az_rg.name
-  description = "The name of the resource group"
-}
-
-output "az_rg_location" {
-  value       = azurerm_resource_group.az_rg.location
-  description = "The location/region of the resource group"
-}
--- test/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "az_rg_name" {
-  value       = module.azure_resource_group.az_rg_name
-  description = "The name of the resource group"
-}
-
-output "az_rg_location" {
-  value       = module.azure_resource_group.az_rg_location
-  description = "The location/region of the resource group"
-}
